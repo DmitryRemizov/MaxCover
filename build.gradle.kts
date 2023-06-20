fb0d143c3812265d5df9069e6e4561356a69c8a6
Dmitry Remizov <rdk.7777@yahoo.com>
2023-06-20 22:09:29 +0300

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.21"
+    //https://www.jacoco.org/jacoco/
+    jacoco
+}
+
+group = "ru.netology."
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    // https://mvnrepository.com/artifact/junit/junit
+    testImplementation("junit:junit:4.13.1")
+}
+
+tasks.test {
+    useJUnit()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
